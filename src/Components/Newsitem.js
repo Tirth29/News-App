7ac88c71c7b97a24b33460ea8ad8741a5fb11cad
Tirth <tirthsolanki29923@gmail.com>
2022-12-01 23:56:09 +0530

    try to add source button

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-    let { title, description, imageUrl,newsUrl, author ,date} = this.props;
+    let { title, description, imageUrl,newsUrl, author ,date, source} = this.props;
     return (
       <div>
         <div className="card" >
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title} <span className="position-absolute top-0 translate-middle round-pill bg-danger" style={{left:'90%', zIndex:1 }}>{source}</span></h5>
             <p className="card-text">{description}</p>
             <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {date} </small></p>
             <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">
