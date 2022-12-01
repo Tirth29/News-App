09313f4baa61831276fcd18dc6be01df28a7a08f
Tirth <tirthsolanki29923@gmail.com>
2022-12-01 19:57:07 +0530

    31 v d

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './Components/NavBar';
 import News from './Components/news';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 export default class App extends Component {
   c="john";
   render() {
     return (
+      <Router>
+
       <div>
         <NavBar/>
         {/* Hello, My First Class Based Component {this.c} */}
-        <News pageSize={6} country="in" category="sports"/>
+          <Routes>
+        <Route exact path="/" element={<News pageSize={6} country="in" category="general"/>}/>
+          <Route exact path="/bussiness"    element={<News pageSize={6} country="in" category="bussiness" key="bussiness"/>}/>
+          <Route exact path="/entertainment" element={<News pageSize={6} country="in" category="entertainment" key="entertainment"/>}/>
+          <Route exact path="/general"      element={<News pageSize={6} country="in" category="general" key="general"/>}/>
+          <Route exact path="/health" element={<News pageSize={6} country="in" category="health" key="health"/>}/>
+          <Route exact path="/science" element={<News pageSize={6} country="in" category="science" key="science"/>}/>
+          <Route exact path="/sports" element={<News pageSize={6} country="in" category="sports" key="sports"/>}/>
+          <Route exact path="/technology" element={<News pageSize={6} country="in" category="technology" key="technology"/>}/>
+          </Routes>
       </div>
+      </Router>
     )
   }
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 export class NavBar extends Component {
   static defaultProps = {
     country: "in",
@@ -17,38 +18,37 @@ export class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg bg-info">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/"><h3>News-Monkey</h3></a>
+    <Link className="navbar-brand" to="/"><h3>News-Monkey</h3></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/"><h5>Home</h5></a>
+          <Link className="nav-link active" aria-current="page" to="/"><h5>Home</h5></Link>
         </li>
-        <li className="nav-item"><a className="nav-link" href="/about"><h5>About</h5></a></li>
-
-        <li className="nav-item"><a className="nav-link" href="/about"><h5>bussiness</h5></a></li>
-        <li className="nav-item"><a className="nav-link" href="/about"><h5>entertainment</h5></a></li>
-        <li className="nav-item"><a className="nav-link" href="/about"><h5>general</h5></a></li>
-        <li className="nav-item"><a className="nav-link" href="/about"><h5>health</h5></a></li>
-        <li className="nav-item"><a className="nav-link" href="/about"><h5>science</h5></a></li>
-        <li className="nav-item"><a className="nav-link" href="/about"><h5>sports</h5></a></li>
-        <li className="nav-item"><a className="nav-link" href="/about"><h5>technology</h5></a></li>
+        <li className="nav-item"><Link className="nav-link" to="/about"><h5>About</h5></Link></li>
+        {/* <li className="nav-item"><Link className="nav-link" to="/bussiness"><h5>bussiness</h5></Link></li> */}
+        <li className="nav-item"><Link className="nav-link" to="/entertainment"><h5>entertainment</h5></Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/general"><h5>general</h5></Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/health"><h5>health</h5></Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/science"><h5>science</h5></Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/sports"><h5>sports</h5></Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/technology"><h5>technology</h5></Link></li>
 
         {/* <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
-          </a>
+          </link>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
+            <li><link className="dropdown-item" to="/">Action</link></li>
+            <li><link className="dropdown-item" to="/">Another action</link></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
+            <li><link className="dropdown-item" to="/">Something else here</link></li>
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
+          <link className="nav-link disabled">Disabled</link>
         </li>  */}
       </ul>
       <form className="d-flex" role="search">
